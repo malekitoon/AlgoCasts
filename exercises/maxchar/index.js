5f1d9b4494be20261b9b265b2fe58cb7d2df4ee7
malekitoon <malekitoon@gmail.com>
2022-06-03 00:34:35 -0600

    4. maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -7,4 +7,28 @@
 
 function maxChar(str) {}
 
+/* Варіант 1: (найкращий)
+ 1) створити пустий обʼєкт
+ 2) пробігтися циклом "for of" по строці і записати к-сть повторень символів в обʼєкт
+ 3) пробігтися циклом "for in" по обʼєкту порівнюючи яке значення найбільше зустрічалося
+*/
+// function maxChar(str) {
+//   const charMap = {};
+//   let max = 0;
+//   let maxChar = '';
+//
+//   for (let char of str) {
+//     charMap[char] = charMap[char] + 1 || 1;
+//   }
+//
+//   for (let char in charMap) {
+//     if (charMap[char] > max) {
+//       max = charMap[char];
+//       maxChar = char;
+//     }
+//   }
+//
+//   return maxChar;
+// }
+
 module.exports = maxChar;
