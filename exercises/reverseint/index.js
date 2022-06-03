5357801da77cffdfdf5980ce4091596573120555
malekitoon <malekitoon@gmail.com>
2022-06-02 22:29:52 -0600

    3. reverseint

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -10,4 +10,19 @@
 
 function reverseInt(n) {}
 
+/* Варіант 1: (найкращий)
+ 1) перетворити строку у масив через split('')
+ 2) перевернути масив arr.reverse() // змінює існуючий масив
+ 3) склеїти масив в строку через join('')
+ 4) розпарсити int зі строки (проігнорує "-" в кінці строки) і домножити на Math.sign(n)
+
+ Math.sign(n) ==> 1
+ Math.sign(-n) ==> -1
+ Math.sign(0) ==> 0
+*/
+// function reverseInt(n) {
+//   const reversed = n.toString().split('').reverse().join('');
+//   return parseInt(reversed) * Math.sign(n);
+// }
+
 module.exports = reverseInt;
