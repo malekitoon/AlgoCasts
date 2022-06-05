20ad5c822c2aa7abfe0ca77d3dff1686b6d48871
malekitoon <malekitoon@gmail.com>
2022-06-04 20:15:06 -0600

    16. stack

--- exercises/stack/index.js
+++ exercises/stack/index.js
@@ -10,6 +10,25 @@
 //   s.pop(); // returns 2
 //   s.pop(); // returns 1
 
-class Stack {}
+// class Stack {}
+
+/* Варіант 1: (найкращий) push/pop  */
+class Stack {
+  constructor() {
+    this.data = [];
+  }
+
+  push(item) {
+    this.data.push(item);
+  }
+
+  pop() {
+    return this.data.pop();
+  }
+
+  peek() {
+    return this.data[this.data.length - 1];
+  }
+}
 
 module.exports = Stack;
